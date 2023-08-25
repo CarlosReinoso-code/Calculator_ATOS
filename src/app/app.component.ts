bb2824e725ae80b6c278673e868ca3800f36b4f3
Carlos Reinoso <creidia204@iesmartinezm.es>
2023-08-25 22:24:11 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraATOSApp';
+  title = 'CALCULADORA ATOS CORP.';
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+
+  sumar(): void {
+    this.result = this.num1 + this.num2;
+  }
+
+  restar(): void {
+    this.result = this.num1 - this.num2;
+  }
+
+  multiplicar(): void {
+    this.result = this.num1 * this.num2;
+  }
+
+  dividir(): void {
+    this.result = this.num1 / this.num2;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
